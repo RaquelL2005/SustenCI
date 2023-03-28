31b4799f6236d19c197995c1215461173e1cc466
Raquell2005 <raqdezotti@gmail.com>
2023-03-28 20:21:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/sustenci/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.etecia.sustenci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity( new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        }, 4000);
+    }
+}
