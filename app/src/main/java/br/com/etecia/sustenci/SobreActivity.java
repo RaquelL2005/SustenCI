0736b1617039ad8d560ace057886d3bd563be71d
Raquell2005 <raqdezotti@gmail.com>
2023-03-28 22:10:36 -0300

    Toolbar

--- app/src/main/java/br/com/etecia/sustenci/SobreActivity.java
+++ app/src/main/java/br/com/etecia/sustenci/SobreActivity.java
@@ -2,13 +2,27 @@ package br.com.etecia.sustenci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class SobreActivity extends AppCompatActivity {
-
+    Toolbar tlbSobre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sobre_layout);
+
+        tlbSobre = findViewById(R.id.idToolbar);
+
+        tlbSobre.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(getApplicationContext(), MenuActivity.class));
+            }
+        });
     }
 }
