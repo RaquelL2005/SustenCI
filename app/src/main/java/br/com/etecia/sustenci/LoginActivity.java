de6448c5509f1f54c2c24aa992e46393254c9efa
Raquell2005 <raqdezotti@gmail.com>
2023-03-28 21:40:17 -0300

    ghghjgj

--- app/src/main/java/br/com/etecia/sustenci/LoginActivity.java
+++ app/src/main/java/br/com/etecia/sustenci/LoginActivity.java
@@ -2,13 +2,50 @@ package br.com.etecia.sustenci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        edtUsuario = findViewById(R.id.idUsuario);
+        edtSenha = findViewById(R.id.idSenha);
+        btnEntrar = findViewById(R.id.idEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usuario, senha;
+
+                usuario = edtUsuario.getText().toString();
+                senha = edtSenha.getText().toString();
+
+                if (usuario.equals("vitor") && senha.equals("22038")) {
+
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Senha e/ou Usuario errados!!", Toast.LENGTH_LONG).show();
+                    limparJanela();
+                }
+            }
+
+            });
     }
+    public void limparJanela() {
+            edtUsuario.setText("");
+            edtSenha.setText("");
+            edtUsuario.requestFocus();
+    }
+
 }
--- app/src/main/java/br/com/etecia/sustenci/MainActivity.java
+++ app/src/main/java/br/com/etecia/sustenci/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity( new Intent(getApplicationContext(), LoginActivity.class));
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
             }
         }, 4000);
     }
