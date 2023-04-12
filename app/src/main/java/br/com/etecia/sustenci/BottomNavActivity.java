4449067ef2a8da94ad22a10795c49ff84008f69d
iamguiss <guizinhosilvasantos10@gmail.com>
2023-04-11 21:56:16 -0300

    ggghgh

---
+++ app/src/main/java/br/com/etecia/sustenci/BottomNavActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.sustenci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+    }
+}
